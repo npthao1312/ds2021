d37f1fb7dbc3c40970411572b7eb67b865eddf7e
chalalala <chalalala@pop-os.localdomain>
2021-02-26 23:19:14 +0700

    Upload code for file transfer

---
+++ labwork1/code/client.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#define SIZE 1024
+
+void write_file(char* content){
+	FILE *fp;
+	fp = fopen("sample.txt", "w");
+	fprintf(fp, content);
+	fclose(fp);
+}
+
+void send_file(FILE *fp, int sockfd){
+    int n;
+    char data[SIZE] = {0};
+
+    while (fgets(data, SIZE, fp) != NULL){
+    	if (send(sockfd, data, sizeof(data), 0) == -1){
+			perror("Can't send the file!");
+			exit(1);
+		}
+		bzero(data, SIZE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int so;
+    char s[100];
+    FILE *fp;
+    struct sockaddr_in ad;
+
+    socklen_t ad_length = sizeof(ad);
+    struct hostent *hep;
+
+    // create socket
+    int serv = socket(AF_INET, SOCK_STREAM, 0);
+
+    // init address
+    hep = gethostbyname(argv[1]);
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr = *(struct in_addr *)hep->h_addr_list[0];
+    ad.sin_port = htons(12345);
+
+    // connect to server
+    connect(serv, (struct sockaddr *)&ad, ad_length);
+
+    while (1) {
+        // after connected, client will generate a file 		contain message
+        printf("Message to write to the file: ");
+        gets(s);
+        write_file(s);
+        
+        // read generated file
+        fp = fopen("sample.txt", "r");
+        if (fp == NULL){
+        	perror("Error in reading file.");
+        	exit(1);
+        }
+
+        // send file to the server
+        printf("Sending file...\n");
+		send_file(fp, serv);
+		printf("Sent successfully.\n");
+
+        // then close connection
+		close(serv);
+
+		return 0;
+    }
+}
+
---
+++ labwork1/code/server.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#define SIZE 1024
+
+void write_file(int sockfd){
+	int n;
+	char buffer[SIZE];
+	
+	FILE *fp;
+	fp = fopen("sample.txt","w");
+	while (1){
+		n = recv(sockfd, buffer, SIZE, 0);
+		if (n <= 0){
+			break;
+			return;
+		}
+		fprintf(fp, "%s", buffer);
+		bzero(buffer, SIZE);
+	}
+	return;
+}
+
+int main() {
+    int ss, cli, pid;
+    struct sockaddr_in ad;
+    char s[100];
+    socklen_t ad_length = sizeof(ad);
+
+    // create the socket
+    ss = socket(AF_INET, SOCK_STREAM, 0);
+
+    // bind the socket to port 12345
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr.s_addr = INADDR_ANY;
+    ad.sin_port = htons(12345);
+    bind(ss, (struct sockaddr *)&ad, ad_length);
+
+    // then listen
+    listen(ss, 0);
+
+    while (1) {
+        // an incoming connection
+        cli = accept(ss, (struct sockaddr *)&ad, &ad_length);
+
+        pid = fork();
+        if (pid == 0) {
+            printf("client connected\n");
+            write_file(cli);
+            printf("Received file.");
+            return 0;
+        }
+        else {
+            // continue the loop to accept more clients
+            continue;
+        }
+    }
+    
+    // disconnect
+    close(cli);
+
+}
